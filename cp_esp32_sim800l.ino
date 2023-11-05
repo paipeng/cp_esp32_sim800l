b356ff6dc423008d46b9390e71a3e5c3e9a69676
Pai Peng <sipaipv6@gmail.com>
2023-11-05 09:01:18 +0800

    use softwareserial (espsoftwareserial)

--- cp_esp32_sim800l.ino
+++ cp_esp32_sim800l.ino
@@ -1,76 +1,43 @@
+#include <SoftwareSerial.h>
+//Create software serial object to communicate with SIM800L
+SoftwareSerial sim800l(16, 17); 
 
-#if 1
-// Configure TinyGSM library
-#define TINY_GSM_MODEM_SIM800      // Modem is SIM800
-#define TINY_GSM_RX_BUFFER   1024  // Set RX buffer to 1Kb
-#include <TinyGsmClient.h>
-// SIM card PIN (leave empty, if not defined)
-const char simPIN[]   = "";
-
-// Your phone number to send SMS: + (plus sign) and country code, for Portugal +351, followed by phone number
-// SMS_TARGET Example for Portugal +351XXXXXXXXX
-#define SMS_TARGET  "+8613233696233"
-
-
-#define MODEM_TX             17
-#define MODEM_RX             16
-
-// Set serial for debug console (to Serial Monitor, default speed 115200)
-#define SerialMon Serial
-// Set serial for AT commands (to SIM800 module)
-#define SerialAT  Serial1
-
-// Define the serial console for debug prints, if needed
-//#define DUMP_AT_COMMANDS
-
-#ifdef DUMP_AT_COMMANDS
-  #include <StreamDebugger.h>
-  StreamDebugger debugger(SerialAT, SerialMon);
-  TinyGsm modem(debugger);
-#else
-  TinyGsm modem(SerialAT);
-#endif
-#endif
 void setup() {
+  delay(1000);
+
   Serial.begin(115200);
   delay(300);
-#if 1
-  // Set GSM module baud rate and UART pins
-  SerialAT.begin(115200, SERIAL_8N1, MODEM_RX, MODEM_TX);
-  delay(3000);
 
-  // Restart SIM800 module, it takes quite some time
-  // To skip it, call init() instead of restart()
-  SerialMon.println("Initializing modem...");
-  if (modem.restart()) {
-    SerialMon.println("success!");
+  //Begin serial communication with Arduino and SIM800L
+  sim800l.begin(115200);
 
+  Serial.println("Initializing...");
+  delay(1000);
 
-    // use modem.init() if you don't need the complete restart
-
-    // Unlock your SIM card with a PIN if needed
-    if (strlen(simPIN) && modem.getSimStatus() != 3 ) {
-      modem.simUnlock(simPIN);
-    }
-
-    // To send an SMS, call modem.sendSMS(SMS_TARGET, smsMessage)
-    String smsMessage = "Hello from ESP32!";
-    if(modem.sendSMS(SMS_TARGET, smsMessage)){
-      SerialMon.println(smsMessage);
-    }
-    else{
-      SerialMon.println("SMS failed to send");
-    }
-
-  } else {
-    SerialMon.println("failed!");
-  }
-
-#endif  
+  sim800l.println("AT"); //Once the handshake test is successful, it will back to OK
+  updateSerial();
+  sim800l.println("AT+CSQ"); //Signal quality test, value range is 0-31 , 31 is the best
+  updateSerial();
+  sim800l.println("AT+CCID"); //Read SIM information to confirm whether the SIM is plugged
+  updateSerial();
+  sim800l.println("AT+CREG?"); //Check whether it has registered in the network
+  updateSerial();
 }
 
 
 void loop() {
-  Serial.println("loop...");
-  delay(1000);
+  updateSerial();
+}
+
+void updateSerial()
+{
+  delay(500);
+  while (Serial.available()) 
+  {
+    sim800l.write(Serial.read());//Forward what Serial received to Software Serial Port
+  }
+  while(sim800l.available()) 
+  {
+    Serial.write(sim800l.read());//Forward what Software Serial received to Serial Port
+  }
 }
